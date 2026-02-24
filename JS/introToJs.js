bac70e77c09009aca88c1a8a28cd51cb2fa4da07
GauravSainiOnline <gauravsaini38538@gmail.com>
2026-02-24 10:27:29 +0530

    Js

---
+++ JS/introToJs.js
@@ -0,0 +1,8 @@
+// Js is a cross Platform programming language 
+// Js is Event-Driven, Client-Side Scripting: JavaScript runs on the user's browser
+// JavaScript is a single-threaded language that executes one task at a time.
+// It is an interpreted language which means it executes the code line by line.
+// The data type of the variable is decided at run-time in JavaScript, which is why it is called dynamically typed.
+
+// Platform independent, Object Oriented
+
