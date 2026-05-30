60c26bca549e1db6b7fcfdb750d12a194623a105
GauravSainiOnline <gauravsaini38538@gmail.com>
2026-05-30 11:24:50 +0530

    conditional rendering in react

---
+++ React Project/src/components1/Weather.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Weather({ temperature }) {
+
+    if (temperature < 15) {
+        return <>
+            <h1>It's Cold Outside!</h1>
+        </>
+    } else if (temperature<25 && temperature > 15) {
+        return <>
+            <h1>It's Nice Outside!
+            </h1>
+        </>
+    }
+    else if (temperature > 25) {
+        return <>
+            <h1>It's Hot Outside!</h1>
+        </>
+    }
+
+}
+
+export default Weather;
