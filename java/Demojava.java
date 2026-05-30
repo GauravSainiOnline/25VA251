d98aa1a1159c656d3754f6b50d86dcba89395426
GauravSainiOnline <gauravsaini38538@gmail.com>
2026-05-30 11:20:12 +0530

    Java basics

---
+++ java/Demojava.java
@@ -0,0 +1,10 @@
+public class Demojava{
+    
+       public static void main(String[] args ){
+        System.out.println("Gaurav Saini");
+        int age = 20;
+        System.out.println("Age: " +  age);
+       }
+
+}
+
